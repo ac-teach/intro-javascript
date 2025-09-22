a4ad57daf565480477cdf5a6c9f561d535b59fd5
Alvaro Calderon - 2006576 <alvarocalderon@kinal.edu.gt>
2025-09-22 12:02:21 -0600

    002 - Agregamos elementos básicos de js

---
+++ contenido/1-variables.js
@@ -0,0 +1,18 @@
+//Buenas practicas, ser descriptivo
+let cajaAudifonos = 'derecho'
+let urlDeUsuario = 'https://www.google.com'
+let carnetAlumno = '2006576'
+
+//Mala práctica, no usar variables ambiguas
+let c = 'derecho' 
+let ca = 'derecho'
+let caUsuario = 'derecho'
+
+console.log(cajaAudifonos)
+
+// otras formas
+var nombre = "Alvaro";
+nombre = "Carlos";
+var edad;
+
+const PI = 3.14159
---
+++ contenido/2-tipos-de-datos.js
@@ -0,0 +1,47 @@
+/**
+ * TIPOS DE DATOS: (1) primitivos y (2) complejos [10 tipos]
+ */
+
+//(1) PRIMITIVOS
+// string
+let nombre = 'Charly'
+
+// number
+let edad = 31
+
+// boolean
+let esMayorDeEdad = true
+
+//null
+let noHayValor = null
+
+//undefined
+let noDefinido = undefined
+
+//symbol
+let simboloUnico = Symbol('unico')
+
+//bigint
+let numeroGrande = 2n
+//console.log(numeroGrande)
+
+
+//COMPLEJOS
+
+//object
+let carro = {
+    marca: 'fiesta',
+    color: 'blanco',
+    duenio: 'mamon'
+}
+
+// array
+let frutas = ['manzana', 'banano', 'uvas']
+
+// function
+function name(params) {
+    //codigo
+}
+console.log(frutas)
+
+
